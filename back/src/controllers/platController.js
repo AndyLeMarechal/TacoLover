e820e9ba255f707737c3f1a0baa284a322d9af96
AndyLeMarechal <andy.le-marechal@oclock.school>
2023-12-14 10:41:05 +0100

    Controllers: Add controller for Plat / Routers: Add router for Plat

---
+++ back/src/controllers/platController.js
@@ -0,0 +1,40 @@
+import { Plat } from "../models/index.js";
+
+export async function getAllPlats(req, res) {
+    const plats = await Plat.findAll()
+    res.json(plats)
+};
+
+export async function getOnePlat(req, res) {
+    const platId = req.params.id;
+    const plat = await Plat.findByPk(platId);
+    res.json(plat);
+};
+
+export async function createdPlat(req, res) {
+    const body = req.body;
+    const createdPlat = await Plat.create({
+        title: body.title,
+        description: body.description
+    });
+    res.json(createdPlat)
+};
+
+export async function updatedPlat(req, res) {
+    const platId = req.params.id;
+    const body = req.body;
+    const plat = await Plat.findByPk(platId);
+    const updatedPlat = await plat.update({
+        title: body.title,
+        description: body.description
+    });
+    res.json(updatedPlat)
+};
+
+export async function deletedPlat(req, res) {
+    const platId = req.params.id;
+    const plat = await Plat.findByPk(platId);
+    const deletedPlat = await plat.destroy();
+    res.json(deletedPlat);
+
+};
---
+++ back/src/routers/platRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express"
+
+import * as platController from "../controllers/platController.js";
+
+export const router = Router();
+
+router.get('/plats', platController.getAllPlats);
+router.get('/plats/:id', platController.getOnePlat);
+router.post('/plats', platController.createdPlat);
+router.patch('/plats/:id', platController.updatedPlat);
+router.delete('/plats/:id', platController.deletedPlat);
