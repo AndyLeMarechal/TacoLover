ebe591246050b5efcb07167b23f401ef1754e588
AndyLeMarechal <andy.le-marechal@oclock.school>
2024-01-04 13:11:29 +0100

    Routers: Add Validate middlewares for Dessert and Drink

--- back/src/routers/drinkRouter.js
+++ back/src/routers/drinkRouter.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import validateMliddlewareFactory from "../../middlewares/validate.mliddleware.factory.js";
+import postDrink from "../../middlewares/schemas/postDrink.js";
+import patchDrink from "../../middlewares/schemas/patchDrink.js";
 
 import * as drinkController from "../controllers/drinkController.js";
 
@@ -6,6 +9,6 @@ export const router = Router();
 
 router.get('/drinks', drinkController.getAllDrinks);
 router.get('/drinks/:id', drinkController.getOneDrink);
-router.post('/drinks', drinkController.createdDrink);
-router.patch('/drinks/:id', drinkController.updatedDrink);
+router.post('/drinks', validateMliddlewareFactory('body', postDrink), drinkController.createdDrink);
+router.patch('/drinks/:id', validateMliddlewareFactory('body', patchDrink), drinkController.updatedDrink);
 router.delete('/drinks/:id', drinkController.deletedDrink);
